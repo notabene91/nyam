524e6ed66a1fc23b60c8d3ad74f695942679ae8d
Александр Шабалин <notabene1991@yandex.ru>
2020-08-28 07:25:23 +0300

    Updates

---
+++ precache-manifest.509ffaaadf4bb07127bb959d96a51d8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4caeb1edad810fad53afaeaae63cb036",
+    "url": "/nyam/index.html"
+  },
+  {
+    "revision": "0f35eb43ae15b2bf5cbe",
+    "url": "/nyam/static/css/main.42ac705b.chunk.css"
+  },
+  {
+    "revision": "8da886af91d64402c304",
+    "url": "/nyam/static/js/2.3a50393a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nyam/static/js/2.3a50393a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f35eb43ae15b2bf5cbe",
+    "url": "/nyam/static/js/main.e9a37d99.chunk.js"
+  },
+  {
+    "revision": "d09eb64453433e6ed478",
+    "url": "/nyam/static/js/runtime-main.46720978.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/nyam/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "a18b1ce2fb80c8b54f1dd05fac5ca903",
+    "url": "/nyam/static/media/background.a18b1ce2.jpg"
+  },
+  {
+    "revision": "1078bfe36a610ad408621570c4468cbd",
+    "url": "/nyam/static/media/cat.1078bfe3.png"
+  }
+]);

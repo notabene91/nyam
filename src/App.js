174213a331eae379c7caa4fcf5fd4ee0004e98ed
Александр Шабалин <notabene1991@yandex.ru>
2020-08-28 07:12:46 +0300

    base functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import './fonts/fonts.css'
+import Card from './components/Card'
 
 function App() {
+
+  const data = [
+    { title: 'Нямушка', subtitle: 'с фуа-гра', portions: '10  ', textPortions: 'порций', mouse: '1 ', textMouse: 'мышь в подарок', weight: '0.5', product: 'Печень утки разварная с артишоками.', disabled: false },
+    { title: 'Нямушка', subtitle: 'с рыбой', portions: '40  ', textPortions: 'порций', mouse: '2 ', textMouse: 'мыши в подарок', weight: '2', product: 'Головы щучьи с чесноком да свежайшая сёмгушка.', disabled: true },
+    { title: 'Нямушка', subtitle: 'с курой', portions: '100  ', textPortions: 'порций', mouse: '5 ', textMouse: 'мышей в подарок, заказчик доволен', weight: '5', product: 'Филе из цыплят с трюфелями в бульоне.', disabled: false },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1 className="main__title">Ты сегодня покормил кота?</h1>
+      <div className="main__container">
+        {data.map(({ title, subtitle, portions, textPortions, mouse, textMouse, weight, product, disabled }) => {
+          return <Card
+            key={subtitle}
+            title={title}
+            subtitle={subtitle}
+            portions={portions}
+            textPortions={textPortions}
+            mouse={mouse}
+            textMouse={textMouse}
+            weight={weight}
+            product={product}
+            disabled={disabled}
+          />
+        })
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import './Card.css';
+import cat from '../images/cat.png'
+
+
+class Card extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: false,
+      isMouseLeave: false,
+      cardClass: "card_active",
+      ovalClass: "card__oval_active",
+      overTitle: "Сказочное заморское лакомство"
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.mouseLeave = this.mouseLeave.bind(this);
+  }
+
+  handleClick() {
+    const { isActive } = this.state;
+    const { disabled } = this.props;
+    if (disabled) {
+      this.setState({ isActive: false });
+    }
+    else {
+      this.setState({ isActive: !isActive });
+      this.setState({ overTitle: "Сказочное заморское лакомство" });
+      this.setState({ isMouseLeave: false })
+      this.setState({ cardClass: "card_active" })
+      this.setState({ ovalClass: "card__oval_active" })
+    }
+  }
+
+  mouseLeave() {
+    const { isActive } = this.state;
+    this.setState({ isMouseLeave: true })
+    if (isActive) {
+      this.setState({ overTitle: "Котэ не одобряет?" })
+      this.setState({ cardClass: "card_active-hover" })
+      this.setState({ ovalClass: "card__oval_active-hover" })
+    }
+    else
+      this.setState({ overTitle: "Сказочное заморское лакомство" })
+  }
+
+
+  render() {
+    const { title, subtitle, portions, textPortions, mouse, textMouse, weight, product, disabled } = this.props;
+    const logo = cat;
+    const { isActive, isMouseLeave, cardClass, ovalClass } = this.state;
+    const cardClassName = `card ${isActive ? cardClass : '' || disabled ? "card_disabled" : ''} `;
+    const ovalClassName = `card__oval ${isActive ? ovalClass : '' || disabled ? "card__oval_disabled" : ''}`;
+    const overTitleName = `card__over-title ${isActive && isMouseLeave ? 'card__over-title_active' : ''}`;
+    const descriptionName = `description ${disabled ? "description_disabled" : ''}`
+    const description = `${disabled ? `Печалька, ${subtitle} закончился.` : `${isActive ? product : "Чего сидишь? Порадуй котэ, "}`}`;
+    const buyName = `${disabled ? '' : `${isActive ? '' : 'купи.'}`}`;
+    const disabledWrapper = `${disabled ? 'card__disable-wrapper' : ''}`
+
+
+    return (
+      <div className="element">
+        <div className={cardClassName}
+          onClick={this.handleClick}
+          onMouseLeave={this.mouseLeave}>
+          <div className={disabledWrapper}>
+            <div className="card__background">
+              <p className={overTitleName}>{this.state.overTitle}</p>
+              <h2 className="card__title">{title}</h2>
+              <p className="card__subtitle">{subtitle}</p>
+              <p className="card__text"><span className="card__text card__text_bold">{portions}</span>{textPortions}</p>
+              <p className="card__text"><span className="card__text card__text_bold">{mouse}</span>{textMouse}</p>
+              <img className="card__image" src={logo} alt="Котик." />
+              <div className={ovalClassName}>
+                <p className="card__weight-number">{weight}</p>
+                <p className="card__weight">кг</p>
+              </div>
+            </div>
+          </div>
+        </div >
+        <p className={descriptionName}>{description}
+          <span className="description description_bold"
+            onClick={this.handleClick}>{buyName}</span>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Card;
